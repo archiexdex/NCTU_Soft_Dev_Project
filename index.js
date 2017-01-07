515341b7cb49959bdf33007979ed6259a1e54e0c
blackboxo <592276898@qq.com>
2017-01-07 11:54:08 +0800

    add Start page

--- index.js
+++ index.js
@@ -9,46 +9,61 @@ let interval = 1000;
 let list = [];
 let index = 0;
 
+var bg;
+var start_button;
+var title;
+var maker;
+var TitleView = new createjs.Container();
+
 function init() {
 	stage = new createjs.Stage("demoCanvas");
+	stage.canvas.width = window.innerWidth;
+  stage.canvas.height = window.innerHeight;
 
-	playerScore = new createjs.Text('0', 'bold 20px Arial', '#A3FF24');
-	playerScore.x = 211;
-	playerScore.y = 20;
-	stage.addChild(playerScore);
+	stage.mouseEventsEnabled = true;
 
-	viewSetting();
+	bg = new createjs.Bitmap("bg.jpg");
+	bg.scaleX=1.4;
+	bg.scaleY=1.2;
 
+	stage.addChild(bg);
 
-	// for (var i = 0; i < 10; i++){
-	// 	list.push();
-	// }
-	circleUp=getCircle("up");
-	circleDown=getCircle("down");
-	circleLeft=getCircle("left");
-	circleRight=getCircle("right");
-
-	stage.addChild(circleUp);
-	stage.addChild(circleDown);
-	stage.addChild(circleLeft);
-	stage.addChild(circleRight);
-
-	createjs.Tween.get(circleUp, {loop: true})
-	  .to({y: -100}, interval);
-	createjs.Tween.get(circleDown, {loop: true})
-	  .to({y: 700}, interval);
-	createjs.Tween.get(circleLeft, {loop: true})
-	  .to({x: -100}, interval);
-	createjs.Tween.get(circleRight, {loop: true})
-	  .to({x: 1100}, interval);
 	createjs.Ticker.setFPS(60);
 	createjs.Ticker.addEventListener("tick", stage);
+	addTitleView();
 
-};
-
-
+}
 
 // Function
+
+function addTitleView(){
+
+
+
+	title = new createjs.Text('Musix', 'bold 120px Arial', '#0099CC');
+	title.x=window.innerWidth/2-167;
+	title.y=window.innerHeight/2-220;
+	start_button = new createjs.Text('Start', 'bold 60px Arial', '#0099CC');
+	start_button.x=window.innerWidth/2-70;
+	start_button.y=window.innerHeight/2;
+
+	maker = new createjs.Text('Made by  賴奕融 楊信之 吳斌', '20px Arial', '#3399CC');
+	maker.x=window.innerWidth/2-130;
+	maker.y=window.innerHeight/2+160;
+
+	TitleView.addChild(start_button, title,maker);
+  stage.addChild(TitleView);
+	stage.update();
+
+	start_button.addEventListener("click",tweenTitleView);
+}
+
+function tweenTitleView(){
+    // Start Game
+    createjs.Tween.get(TitleView).to({y:-520}, 300).call(viewSetting);
+}
+
+
 function getCircle(mode) {
 	let tmp = new createjs.Shape();
 	switch (mode) {
@@ -71,6 +86,31 @@ function getCircle(mode) {
 }
 
 function viewSetting() {
+	playerScore = new createjs.Text('0', 'bold 20px Arial', '#A3FF24');
+	playerScore.x = 211;
+	playerScore.y = 20;
+	stage.addChild(playerScore);
+
+	circleUp=getCircle("up");
+	circleDown=getCircle("down");
+	circleLeft=getCircle("left");
+	circleRight=getCircle("right");
+
+	stage.addChild(circleUp);
+	stage.addChild(circleDown);
+	stage.addChild(circleLeft);
+	stage.addChild(circleRight);
+
+	createjs.Tween.get(circleUp, {loop: true})
+		.to({y: -100}, interval);
+	createjs.Tween.get(circleDown, {loop: true})
+		.to({y: 700}, interval);
+	createjs.Tween.get(circleLeft, {loop: true})
+		.to({x: -100}, interval);
+	createjs.Tween.get(circleRight, {loop: true})
+		.to({x: 1100}, interval);
+
+
 	hitPadUp = new createjs.Shape();
 	hitPadUp.graphics.beginFill("Pink").drawCircle(0, 0, 60);
 	hitPadUp.x = 500;
