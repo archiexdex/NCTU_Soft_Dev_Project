a1720e85394f08b33064461f6cd0e6745b0f815c
XDEX <rc840327@gmail.com>
2017-01-15 11:04:46 +0800

    1. Modify ComboEffect

--- ComboEffect.js
+++ ComboEffect.js
@@ -1,6 +1,6 @@
 class ComboEffect {
 	constructor(comboText) {
-		var tmp = new createjs.Text(comboText, 'bold 50px Arial', '#A3FF24');
+		var tmp = new createjs.Text(comboText, 'bold 100px Arial', '#FF00FF');
 		tmp.x = window.innerWidth/2-tmp.getMeasuredWidth()/2;
 		tmp.y = window.innerHeight/2-tmp.getMeasuredHeight()/2;
 		return tmp;
