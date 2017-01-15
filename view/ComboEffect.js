9e04bb30c8a3de5e1710b5fab7b71bff2fac7484
XDEX <rc840327@gmail.com>
2017-01-15 19:13:49 +0800

    1. Modify text size

--- view/ComboEffect.js
+++ view/ComboEffect.js
@@ -1,6 +1,6 @@
 class ComboEffect {
 	constructor(comboText) {
-		var tmp = new createjs.Text(comboText, 'bold 80px Arial', '#FF00FF');
+		var tmp = new createjs.Text(comboText, 'bold 40px Arial', '#FF00FF');
 		return tmp;
 	}
 }
